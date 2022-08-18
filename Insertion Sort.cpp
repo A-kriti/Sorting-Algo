79bd36a560014f6baf0c5d9d661c6ff79bb44531
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-08-18 15:58:09 +0530

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -18,6 +18,22 @@ vector<int> Insertion_Sort(vector<int>v){
     return v;
 }
 
+
+void InsertionSort(vector<int>&v){
+    
+    int n=v.size();
+    for(int i=0;i<n;i++){
+        int j=i;
+        int temp=v[i];
+        while(j>0 && v[j-1]>temp){
+            v[j]=v[j-1];
+            j--;
+        }
+        
+        v[j]=temp;
+    }
+}
+
 int main()
 {
     vector<int>v={5,1,3,0,2};
